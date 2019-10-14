80bfb74855587714beac82fa71ec2123af4129f5
skyroutes <51452899+skyroutes@users.noreply.github.com>
2019-10-14 17:05:05 +0530

    2.1
    optimizations

--- sw.js
+++ sw.js
@@ -1,19 +1,243 @@
-
 /*
+    SKYROUTES 2.0.1
 
-
-    ___         __              __       ____                  __
-   /   | ____ _/ /______ ______/ /_     / __ \____ _____  ____/ /__  __  __
-  / /| |/ __ `/ //_/ __ `/ ___/ __ \   / /_/ / __ `/ __ \/ __  / _ \/ / / /
- / ___ / /_/ / ,< / /_/ (__  ) / / /  / ____/ /_/ / / / / /_/ /  __/ /_/ /
-/_/  |_\__,_/_/|_|\__,_/____/_/ /_/  /_/    \__,_/_/ /_/\__,_/\___/\__, /
-                                                                  /____/
-
-All Rights Reserved : Aakash Pandey : aakash.pandey@live.com : 2019-2020
-
-SKYROUTES v2.0B
-
+    Copyright © 2019 Aakash Pandey. All rights reserved.
+    Use of this source code is governed by a license that can be
+    found in the LICENSE file.
 
 */
 
-let e=[],a=!0,t=!0;const s=async e=>{(await clients.matchAll()).forEach(a=>a.postMessage(e))};self.addEventListener("fetch",s=>{if (s.request.referrer !== location.origin + "/" && ((s.request.url.includes(".html")) || (s.request.referrer.endsWith('.html')) || (s.request.url.endsWith('.css')) || (s.request.url.endsWith('.js'))) && (s.request.referrer.replace(location.origin + '/', '') === "") ? true : false) {s.respondWith(new Response(`<html><script>window.location.href='${location.origin}'</script></html>`, {headers: {'Content-Type': 'text/html'}}))};const i=new URL(s.request.url);if(i.origin===location.origin&&"navigate"===s.request.mode&&!i.pathname.includes(".")&&a){a=!0;var n=caches.match(location.origin+"/index.html");s.respondWith(n)}else e.includes(i.pathname)&&t&&s.respondWith(caches.match(i))}),self.addEventListener("message",async i=>{let n=i.data.req,l=i.data.data;if("ping"===n)s({req:"pong"});else if("touch"===n)(e=l||[]).push("/index.html","/lib/skyroutes.css","/lib/skyroutes.js","/main.js"),a=!0;else if("init"===n)(e=l||[]).push("/index.html","/lib/skyroutes.css","/lib/skyroutes.js","/main.js"),cache=await caches.open("2.0"),await cache.addAll(e),s({req:"load"});else if("upsw"===n){let e=await caches.keys();await Promise.all(e.map(e=>{if(!"2.0".includes(e))return caches.delete(e)})),self.skipWaiting()}else if("gracenav"===n)a=!1;else if("cacheoff"===n)t=!1;else if("cacheon"===n)t=!0;else if("atomic"===n){s({req:"cacheoff"});let a=await(async()=>{let a=[],t="";for(fname of e)if(cfile=await caches.match(fname))if(fname.endsWith(".js")||fname.endsWith(".css")||fname.endsWith(".json")||fname.endsWith(".txt")||fname.endsWith(".html")||fname.endsWith(".csv")||fname.includes("fonts")){cfile=await cfile.text();let e=await fetch(fname);e=await e.text(),"/main.js"===fname&&(t=e.toString()),cfile.toString()!=e.toString()&&a.push(fname)}else{cfile=await cfile.blob();let e=await fetch(fname,{method:"HEAD"});e=e.headers.get("content-length"),cfile.size!==parseInt(e)&&a.push(fname)}return a.includes("/main.js")?{new:a,raw:t}:{new:a}})();if(a.new.length>0)if(a.new.includes("/main.js")){let e=(e=>{try{return c=e.indexOf('"assets"'),e=e.slice(c),c=e.indexOf("["),e=e.slice(c),c=e.indexOf("]"),e=""===(e=(e=(e=e.slice(1,c)).includes("'")?e.split("'"):e.split('"')).filter(e=>e.includes("/")))||0===e.length?0:e}catch(e){return-1}})(a.raw);s({req:"fsync",data:{cmp:e,add:a.new}})}else s({req:"fsync",data:{add:a.new}});else s({req:"cacheon"}),setTimeout(()=>s({req:"load"}),600)}else"fsync"===n&&await(async e=>{let a=await caches.open("2.0");if(e.add&&e.add.length>0)for(x of e.add)await a.add(x);if(e.delete&&e.delete.length>0)for(x of e.delete)await a.delete(x);s({req:"cacheon"}),setTimeout(()=>s({req:"reboot"}),600)})(l)});
+
+const force_upgrade_string = "Please save me";
+const sr_version = "2.0";
+let AppCache = [];
+let AppManifest;
+let NewAppManifest;
+
+let grab_navigation = true;
+let app_error = false;
+
+let serve_cache = true;
+// ______________________________________________
+
+
+let leak;
+let leaks = [];
+
+
+const northBridge = async (data) => {
+    let cl = await clients.matchAll();
+    cl.forEach(c => c.postMessage(data));
+};
+
+
+// DATA MANAGEMENT
+
+const cacheManger = async (dact) => {
+    // turn off cache serve
+
+    let cache = await caches.open(sr_version);
+
+    if (dact.add) {
+        if (dact.add.length > 0) {
+            for (x of dact.add) {
+                await cache.add(x);
+                console.log(`♻ ${x} synced`);
+            }
+        }
+    }
+
+    if (dact.delete) {
+        if (dact.delete.length > 0) {
+            for (x of dact.delete) {
+                await cache.delete(x);
+                console.log(`🔥 ${x} purged`);
+            }
+        }
+    }
+
+    serve_cache = true;
+    AppCache = [];
+    let cc = await caches.open(sr_version);
+    let curr = await cc.keys();
+    curr.forEach((e => AppCache.push(e.url.replace(location.origin, ''))));
+
+    northBridge({ req: 'update', data: NewAppManifest })
+
+}
+
+const assetScanner = async (iadd) => {
+
+    let root = location.pathname.replace('sw.js', '');
+    let fresh = [];
+    NewAppManifest.skyroutes.assets.map(e => (root==='/') ? fresh.push(`${e}`):fresh.push(`${root}${e}`));
+
+    fresh.push(`${root}index.html`, `${root}lib/skyroutes.css`, `${root}lib/skyroutes.js`);
+
+    let ir = [];
+    let ia = [];
+
+    if (AppCache.toString !== fresh.toString()) {
+        ir = AppCache.filter((e) => (!fresh.includes(e)));
+        ia = fresh.filter((e) => (!AppCache.includes(e)));
+
+    }
+
+    if (iadd) { ia = ia.concat(iadd) }
+
+    if (ia.length !== 0 || ir.length !== 0) {
+
+        cacheManger({ delete: ir, add: ia });
+
+    } else {
+
+        (JSON.stringify(AppManifest) !== JSON.stringify(NewAppManifest)) ? northBridge({ req: 'update', data: NewAppManifest }) : northBridge({ req: 'load' });
+        serve_cache = true;
+    }
+}
+
+const diff = async () => {
+    serve_cache = false;
+
+    let nf = [];
+
+    for (fname of AppCache) {
+        if (cfile = await caches.match(fname)) {
+            if (fname.endsWith('.js') || fname.endsWith('.css') || fname.endsWith('.json') || fname.endsWith('.txt') || fname.endsWith('.html') || fname.endsWith('.csv') || fname.includes('fonts')) {
+                cfile = await cfile.text();
+                let r = await fetch(fname);
+                r = await r.text();
+                (cfile.toString() != r.toString()) ? nf.push(fname) : console.log(`🍏 ${fname} Up to date`);
+            } else {
+                cfile = await cfile.blob();
+                let r = await fetch(fname, { method: 'HEAD' });
+                r = r.headers.get('content-length');
+                (cfile.size !== parseInt(r)) ? nf.push(fname) : console.log(`🍏 ${fname} Up to date`);
+            }
+        }
+    }
+
+    assetScanner(nf);
+}
+
+// Events
+
+self.addEventListener('message', async (e) => {
+
+    let req = e.data.req;
+    let data = e.data.data;
+
+    if (req === 'init') {
+
+        AppManifest = data.manifest;
+        Routes = data.routes;
+        let root = location.pathname.replace('sw.js', '');
+        AppCache = [];
+        AppManifest.skyroutes.assets.forEach(a => (root==='/') ? AppCache.push(`${a}`):AppCache.push(`${root}${a}`));
+        AppCache.push(`${root}index.html`, `${root}lib/skyroutes.css`, `${root}lib/skyroutes.js`);
+
+        cache = await caches.open(sr_version);
+
+        try { await cache.addAll(AppCache); app_error = false }
+        catch (e) {
+            console.error('🛑 Asset file error '+e);
+            app_error = true
+            northBridge({ req: "error", data: "Asset Error" });
+        }
+
+        if (app_error === false){ 
+            console.log("✅ App Init completed");
+            northBridge({ req: "update" });
+        }
+        
+
+    } else if (req === 'atomic' || req === 'touch') {
+
+        if (AppCache.length === 0) {
+            let cc = await caches.open(sr_version);
+            let curr = await cc.keys();
+            curr.forEach((e => AppCache.push(e.url.replace(location.origin, ''))));
+        }
+    
+        AppManifest = data.manifest;
+        console.log('✅ App running');
+
+        if (req === 'atomic') {
+    
+            fetch('manifest.json').then((e) => {
+                e.json().then((ee) => {
+                    NewAppManifest = ee;
+                    diff();
+                });
+            });
+        }
+
+    } else if (req === 'upsw') {
+        let keys = await caches.keys();
+
+        await Promise.all(
+            keys.map((k) => {
+                if (!sr_version.includes(k)) {
+                    return caches.delete(k);
+                }
+            })
+        );
+        self.skipWaiting();
+
+    } else if (req === 'ping') {
+        northBridge({ req: "pong" });
+
+    } else if (req === 'gracenav') {
+        grab_navigation = false;
+    }
+
+});
+
+
+//  jammer gen 5
+
+self.addEventListener('fetch', async e => {
+
+    if (AppCache.length === 0) {
+        let cc = await caches.open(sr_version);
+        let curr = await cc.keys();
+        curr.forEach((e => AppCache.push(e.url.replace(location.origin, ''))));
+    }
+
+    leak = e;
+    
+    if (app_error) {console.error('🛑 [SkyRoutes] App Error, re-check manifest');northBridge({req:'error', data:'App Error, please try later'});}
+
+    // source bounce
+    if (e.request.referrer !== location.origin + "/" && ((e.request.url.includes(".html")) || (e.request.referrer.endsWith('.html')) || (e.request.url.endsWith('.css')) || (e.request.url.endsWith('.js')) || (e.request.url.endsWith('.json'))) && (e.request.referrer.replace(location.origin + '/', '') === "") ? true : false) {
+        e.respondWith(new Response(`<html><script>window.location.href='${location.origin}${location.pathname.replace('sw.js', '')}'</script></html>`, {
+            headers: { 'Content-Type': 'text/html' }
+        }));
+    }
+
+    const url = new URL(e.request.url);
+
+    // return index.html for paths
+
+    leaks.push(url.pathname);
+    
+    if (!app_error && url.origin.includes(location.origin) && e.request.mode === "navigate" && (!url.pathname.includes(".")) && grab_navigation) {
+        
+        var r = caches.match(location.pathname.replace('sw.js', '') + 'index.html');
+        try{
+            e.respondWith(r)
+        } catch (e) {}
+    }
+
+    // cache serve
+    if (!app_error && AppCache.includes(url.pathname) && serve_cache) {
+        console.log(`📦 ${url}`);
+       try {
+            e.respondWith(caches.match(url.pathname))
+       } catch (e) {}
+
+    } else {
+        (!grab_navigation) && (grab_navigation = true);
+        console.log(`🌐 ${url}`);     
+    }
+});
